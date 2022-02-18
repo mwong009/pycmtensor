d88c79f9eb257bca6d10078722ed0331659145b2
Melvin <m.j.w.wong@tue.nl>
2022-02-18 13:07:39 +0100

    new file: results, statistics.py

---
+++ pycmtensor/results.py
@@ -0,0 +1,134 @@
+# results.py
+
+import aesara
+import dill as pickle
+import numpy as np
+import pandas as pd
+from numpy import nan_to_num as nan2num
+
+from pycmtensor.functions import bhhh, hessians
+from pycmtensor.statistics import *
+
+
+class Results:
+    def __init__(self, model, db, show_weights=False):
+        """ Generate the output results to stdout
+
+        Args:
+            model (Class): The model class object or pickle file
+            db (Class): The database class object
+        """
+        if isinstance(model, str):
+            self.name = model
+            with open(self.name, "rb") as f:
+                model = pickle.load(f)
+        else:
+            self.name = model.name
+
+        sample_size = len(db.data)
+        n_params = len(model.get_betas())
+        n_weights = model.get_weight_size()
+        k = n_params + n_weights
+
+        null_loglike = model.null_ll
+        max_loglike = model.best_ll
+
+        self.beta_results = get_beta_statistics(model, db)
+        self.rho_square = nan2num(1.0 - max_loglike / null_loglike)
+        self.rho_square_bar = nan2num(1.0 - (max_loglike - k) / null_loglike)
+        self.ll_ratio_test = -2.0 * (null_loglike - max_loglike)
+
+        self.akaike = 2.0 * (k - max_loglike)
+        self.bayesian = -2.0 * max_loglike + k * np.log(sample_size)
+        self.g_norm = gradient_norm(model, db)
+        self.correlationMatrix = correlation_matrix(model, db)
+        self.model = model
+        self.n_params = n_params
+        self.n_weights = n_weights
+        self.sample_size = sample_size
+        self.best_epoch = model.best_epoch
+        self.best_ll_score = model.best_ll_score
+        self.null_loglike = null_loglike
+        self.max_loglike = max_loglike
+
+        self.print_results = (
+            f"Results for model {self.name}\n"
+            + f"Number of Beta parameters: {self.n_params}\n"
+            + (
+                f"Total size of Neural Net weights: {self.n_weights}\n"
+                if n_weights > 0
+                else ""
+            )
+            + f"Sample size: {self.sample_size}\n"
+            + f"Init loglikelihood: {self.null_loglike:.3f}\n"
+            + f"Final loglikelihood: {self.max_loglike:.3f}\n"
+            + f"Likelihood ratio test: {self.ll_ratio_test:.3f}\n"
+            + f"Accuracy: {(100 * self.best_ll_score):.3f}%\n"
+            + f"Rho square: {self.rho_square:.3f}\n"
+            + f"Rho bar square: {self.rho_square_bar:.3f}\n"
+            + f"Akaike Information Criterion: {self.akaike:.2f}\n"
+            + f"Bayesian Information Criterion: {self.bayesian:.2f}\n"
+            + f"Final gradient norm: {self.g_norm:.3f}\n"
+        )
+
+        print(self.print_results)
+        print(self.beta_results.to_string(), "\n")
+
+        if model.get_weight_size() != 0:
+            self.weights = model.output_estimated_weights()
+            if show_weights:
+                for w, value in zip(model.get_weights(), self.weights):
+                    random = "zeros"
+                    if w.random_init:
+                        random = "random"
+                    print(f"{w.name} {w.size} init: {random}\n{value}\n")
+        else:
+            self.weights = None
+
+    def __str__(self):
+        return self.print_results + self.beta_results.to_string()
+
+
+def get_beta_statistics(model, db):
+    H = aesara.function(
+        inputs=model.inputs,
+        outputs=hessians(model.p_y_given_x, model.y, model.beta_params),
+        on_unused_input="ignore",
+    )
+
+    BHHH = aesara.function(
+        inputs=model.inputs,
+        outputs=bhhh(model.p_y_given_x, model.y, model.beta_params),
+        on_unused_input="ignore",
+    )
+
+    h = H(*db.input_data())
+    bh = BHHH(*db.input_data())
+
+    pandas_stats = pd.DataFrame(
+        columns=[
+            "Value",
+            "Std err",
+            "t-test",
+            "p-value",
+            "Rob. Std err",
+            "Rob. t-test",
+            "Rob. p-value",
+        ],
+        index=[p.name for p in model.beta_params if (p.status != 1)],
+    )
+    stderr = stderror(h, model.beta_params)
+    robstderr = rob_stderror(h, bh, model.beta_params)
+    pandas_stats["Std err"] = stderr
+    pandas_stats["t-test"] = t_test(stderr, model.beta_params)
+    pandas_stats["p-value"] = p_value(stderr, model.beta_params)
+    pandas_stats["Rob. Std err"] = robstderr
+    pandas_stats["Rob. t-test"] = t_test(robstderr, model.beta_params)
+    pandas_stats["Rob. p-value"] = p_value(robstderr, model.beta_params)
+    pandas_stats = pd.DataFrame(index=[p.name for p in model.beta_params]).join(
+        pandas_stats
+    )
+
+    pandas_stats["Value"] = np.asarray(model.output_estimated_betas())
+
+    return np.round(pandas_stats, 6).sort_index().fillna("-").astype("O")
---
+++ pycmtensor/statistics.py
@@ -0,0 +1,125 @@
+# statistics.py
+
+import aesara
+import aesara.tensor as aet
+import numpy as np
+import pandas as pd
+
+from scipy import linalg, stats
+from numpy import nan_to_num as nan2num
+
+from pycmtensor.functions import hessians, bhhh, neg_loglikelihood
+
+
+def variance_covariance(h):
+    return -linalg.pinv(nan2num(h))
+
+
+def rob_variance_covariance(h, bhhh):
+    varCovar = variance_covariance(h)
+    return varCovar.dot(bhhh.dot(varCovar))
+
+
+def t_test(stderr, params):
+    params = [p() for p in params if (p.status != 1)]
+    return [p.eval() / s for p, s in zip(params, stderr)]
+
+
+def p_value(stderr, params):
+    tTest = t_test(stderr, params)
+    return [2.0 * (1.0 - stats.norm.cdf(abs(t))) for t in tTest]
+
+
+def stderror(h, params):
+    params = [p() for p in params if (p.status != 1)]
+    varCovar = variance_covariance(h)
+    stdErr = []
+    for i in range(len(params)):
+        if varCovar[i, i] < 0:
+            stdErr.append(np.finfo(float).max)
+        else:
+            stdErr.append(np.sqrt(varCovar[i, i]))
+
+    return stdErr
+
+
+def rob_stderror(h, bhhh, params):
+    params = [p() for p in params if (p.status != 1)]
+    varCovar = variance_covariance(h)
+    robVarCovar = varCovar.dot(bhhh.dot(varCovar))
+    robstderr = []
+    for i in range(len(params)):
+        if robVarCovar[i, i] < 0:
+            robstderr.append(np.finfo(float).max)
+        else:
+            robstderr.append(np.sqrt(robVarCovar[i, i]))
+
+    return robstderr
+
+
+def correlation_matrix(model, db):
+    H = aesara.function(
+        inputs=model.inputs,
+        outputs=hessians(model.p_y_given_x, model.y, model.beta_params),
+        on_unused_input="ignore",
+    )
+
+    h = H(*db.input_data())
+
+    varCovar = variance_covariance(h)
+    d = np.diag(varCovar)
+    if (d > 0).all():
+        diag = np.diag(np.sqrt(d))
+        diagInv = linalg.inv(diag)
+        matrix = diagInv.dot(varCovar.dot(diagInv))
+    else:
+        matrix = np.full_like(varCovar, np.finfo(float).max)
+
+    df = pd.DataFrame(
+        columns=[p.name for p in model.beta_params if (p.status != 1)],
+        index=[p.name for p in model.beta_params if (p.status != 1)],
+        data=matrix,
+    )
+    return df
+
+
+def rob_correlation_matrix(model, db):
+    H = aesara.function(
+        inputs=model.inputs,
+        outputs=hessians(model.p_y_given_x, model.y, model.beta_params),
+        on_unused_input="ignore",
+    )
+    BH = aesara.function(
+        inputs=model.inputs,
+        outputs=bhhh(model.p_y_given_x, model.y, model.beta_params),
+        on_unused_input="ignore",
+    )
+    robVarCovar = rob_variance_covariance(
+        h=H(*db.input_data()), bhhh=BH(*db.input_data())
+    )
+    rd = np.diag(robVarCovar)
+    if (rd > 0).all():
+        diag = np.diag(np.sqrt(rd))
+        diagInv = linalg.inv(diag)
+        matrix = diagInv.dot(robVarCovar.dot(diagInv))
+    else:
+        matrix = np.full_like(robVarCovar, np.finfo(float).max)
+
+    df = pd.DataFrame(
+        columns=[p.name for p in model.beta_params if (p.status != 1)],
+        index=[p.name for p in model.beta_params if (p.status != 1)],
+        data=matrix,
+    )
+    return df
+
+
+def gradient_norm(model, db):
+    cost = neg_loglikelihood(model.p_y_given_x, model.y)
+    gparams = [p() for p in model.beta_params if (p.status != 1)]
+    grads = aet.grad(cost, gparams, disconnected_inputs="ignore")
+
+    gnorm_fn = aesara.function(
+        inputs=model.inputs, outputs=linalg.norm(grads), on_unused_input="ignore",
+    )
+
+    return gnorm_fn(*db.input_data())
