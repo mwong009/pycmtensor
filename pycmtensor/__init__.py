298db43beadd2f46f8300875377fe875f0bec66e
Melvin <m.j.w.wong@tue.nl>
2022-02-18 11:28:23 +0100

    Bump version: 0.1.0 → 0.2.0

--- pycmtensor/__init__.py
+++ pycmtensor/__init__.py
@@ -1,5 +1,4 @@
 """Top-level package for PyCMTensor."""
 
 __author__ = """Melvin Wong"""
-__email__ = 'm.j.w.wong@tue.nl'
-__version__ = '0.1.0'
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -4,41 +4,41 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Melvin Wong",
-    author_email='m.j.w.wong@tue.nl',
-    python_requires='>=3.6',
+    author_email="m.j.w.wong@tue.nl",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    description="Python Tensor based package for Deep Neural Net assisted Discrete Choice Modelling.",
+    description="Python Tensor based package for Deep neural net assisted Discrete Choice Modelling.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='pycmtensor',
-    name='pycmtensor',
-    packages=find_packages(include=['pycmtensor', 'pycmtensor.*']),
-    test_suite='tests',
+    keywords="pycmtensor",
+    name="pycmtensor",
+    packages=find_packages(include=["pycmtensor", "pycmtensor.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/mwong009/pycmtensor',
-    version='0.1.0',
+    url="https://github.com/mwong009/pycmtensor",
+    version="0.2.0",
     zip_safe=False,
 )
