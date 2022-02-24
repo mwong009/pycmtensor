3064d9e603a80aefda4e9fdffd015e9815649467
Melvin Wong <m.j.w.wong@tue.nl>
2022-02-24 01:26:24 +0100

    refactor(optimizers): refactor base Optimizer class

--- pycmtensor/optimizers.py
+++ pycmtensor/optimizers.py
@@ -7,16 +7,33 @@ import numpy as np
 floatX = aesara.config.floatX
 
 
-class Adam:
-    def __init__(self, params, b1=0.9, b2=0.999, epsilon=1e-8):
+class Optimizer:
+    def __init__(self, params, name, b1=0.0, b2=0.0, m=0.0, rho=0.0, epsilon=1e-8):
+        self.name = name
         params = [p() for p in params if p.status != 1]
         zero = np.array(0.0).astype(floatX)
-        self._m = [aesara.shared(p.get_value() * zero) for p in params]
-        self._v = [aesara.shared(p.get_value() * zero) for p in params]
-        self._t = aesara.shared(zero)
-        self.b1 = b1
-        self.b2 = b2
         self.epsilon = epsilon
+        if (b1 > 0.0) and (b2 > 0.0):
+            self._m = [aesara.shared(p.get_value() * zero) for p in params]
+            self._v = [aesara.shared(p.get_value() * zero) for p in params]
+            self._t = aesara.shared(zero)
+            self.b1 = b1
+            self.b2 = b2
+        if rho > 0.0:
+            self._accu = [aesara.shared(p.get_value() * zero) for p in params]
+            self._delta = [aesara.shared(p.get_value() * zero) for p in params]
+            self.rho = rho
+        if m > 0.0:
+            self._velocity = [aesara.shared(p.get_value() * zero) for p in params]
+            self.m = m
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+class Adam(Optimizer):
+    def __init__(self, params, b1=0.9, b2=0.999):
+        super().__init__(params, name="Adam", b1=b1, b2=b2)
 
     def update(self, cost, params, lr=0.001):
         params = [p() for p in params if p.status != 1]
@@ -29,13 +46,13 @@ class Adam:
         v_prev = self._v
 
         t_new = t + 1.0
-        a_t = aet.sqrt(one - self.b2 ** t_new) / (one - self.b1 ** t_new)
+        a_t = aet.sqrt(one - self.b2**t_new) / (one - self.b1**t_new)
 
         for m, v, param, grad in zip(m_prev, v_prev, params, grads):
             m_t = self.b1 * m + (one - self.b1) * grad
-            v_t = self.b2 * v + (one - self.b2) * grad ** 2
-            g_t = a_t * m_t / (aet.sqrt(v_t) + self.epsilon)
-            p_t = param - lr * g_t
+            v_t = self.b2 * v + (one - self.b2) * grad**2
+            g_t = lr * a_t * m_t / (aet.sqrt(v_t) + self.epsilon)
+            p_t = param - g_t
             updates.append((m, m_t))
             updates.append((v, v_t))
             updates.append((param, p_t))
@@ -45,16 +62,9 @@ class Adam:
         return updates
 
 
-class Adamax:
-    def __init__(self, params, b1=0.9, b2=0.999, epsilon=1e-8):
-        params = [p() for p in params if p.status != 1]
-        zero = np.array(0.0).astype(floatX)
-        self._m = [aesara.shared(p.get_value() * zero) for p in params]
-        self._v = [aesara.shared(p.get_value() * zero) for p in params]
-        self._t = aesara.shared(zero)
-        self.b1 = b1
-        self.b2 = b2
-        self.epsilon = epsilon
+class Adamax(Optimizer):
+    def __init__(self, params, b1=0.9, b2=0.999):
+        super().__init__(params, name="Adamax", b1=b1, b2=b2)
 
     def update(self, cost, params, lr=0.001):
         params = [p() for p in params if p.status != 1]
@@ -67,7 +77,7 @@ class Adamax:
         v_prev = self._v
 
         t_new = t + 1.0
-        a_t = lr / (one - self.b1 ** t)
+        a_t = lr / (one - self.b1**t)
 
         for m, v, param, grad in zip(m_prev, v_prev, params, grads):
             m_t = self.b1 * m + (one - self.b1) * grad
@@ -83,14 +93,9 @@ class Adamax:
         return updates
 
 
-class Adadelta:
-    def __init__(self, params, rho=0.95, epsilon=1e-8):
-        params = [p() for p in params if p.status != 1]
-        zero = np.array(0.0).astype(floatX)
-        self._accu = [aesara.shared(p.get_value() * zero) for p in params]
-        self._delta = [aesara.shared(p.get_value() * zero) for p in params]
-        self.rho = rho
-        self.epsilon = epsilon
+class Adadelta(Optimizer):
+    def __init__(self, params, rho=0.95):
+        super().__init__(params, name="Adadelta", rho=rho)
 
     def update(self, cost, params, lr=1.0):
         params = [p() for p in params if p.status != 1]
@@ -102,11 +107,11 @@ class Adadelta:
         delta = self._delta
         for a, d, param, grad in zip(accumulator, delta, params, grads):
             # update accumulator
-            a_t = self.rho * a + (one - self.rho) * grad ** 2
+            a_t = self.rho * a + (one - self.rho) * grad**2
             # compute parameter update, using previous delta
             g_t = grad * aet.sqrt(d + self.epsilon) / aet.sqrt(a_t + self.epsilon)
             p_t = param - lr * g_t
-            d_t = self.rho * d + (one - self.rho) * g_t ** 2
+            d_t = self.rho * d + (one - self.rho) * g_t**2
             updates.append((param, p_t))
             updates.append((a, a_t))
             updates.append((d, d_t))
@@ -114,14 +119,9 @@ class Adadelta:
         return updates
 
 
-class RMSProp:
-    def __init__(self, params, rho=0.9, epsilon=1e-8, decay=0.0):
-        params = [p() for p in params if p.status != 1]
-        zero = np.array(0.0).astype(floatX)
-        self._a = [aesara.shared(p.get_value() * zero) for p in params]
-        self.rho = rho
-        self.epsilon = epsilon
-        self.decay = decay
+class RMSProp(Optimizer):
+    def __init__(self, params, rho=0.9):
+        super().__init__(params, name="RMSProp", rho=rho)
 
     def update(self, cost, params, lr=0.001):
         params = [p() for p in params if p.status != 1]
@@ -129,11 +129,11 @@ class RMSProp:
         grads = aet.grad(cost, params, disconnected_inputs="ignore")
 
         updates = []
-        accumulators = self._a
-        for a, param, grad in zip(accumulators, params, grads):
-            a_t = self.rho * a + (one - self.rho) * grad ** 2
-            g_t = grad / (aet.sqrt(a_t) + self.epsilon)
-            p_t = param - lr * g_t
+        accumulator = self._accu
+        for a, param, grad in zip(accumulator, params, grads):
+            a_t = self.rho * a + (one - self.rho) * grad**2
+            g_t = lr / aet.sqrt(a_t + self.epsilon) * grad
+            p_t = param - g_t
 
             updates.append((a, a_t))
             updates.append((param, p_t))
@@ -141,29 +141,30 @@ class RMSProp:
         return updates
 
 
-class MomentumSGD:
+class MomentumSGD(Optimizer):
     def __init__(self, params, momentum=0.9, nesterov=False):
-        params = [p() for p in params if p.status != 1]
-        zero = np.array(0.0).astype(floatX)
-        self._moments = [aesara.shared(p.get_value() * zero) for p in params]
-        self.momentum = momentum
+        super().__init__(params, name="MomentumSGD", m=momentum)
         self.nesterov = nesterov
+        if self.nesterov:
+            self.name = "Nesterov"
 
     def update(self, cost, params, lr=0.001):
         params = [p() for p in params if p.status != 1]
-        grads = aet.grad(cost, params, disconnected_inputs="ignore")
 
         updates = []
-        moments = self._moments
-        for m, param, grad in zip(moments, params, grads):
-            velocity = self.momentum * m - lr * grad
+        velocity = self._velocity
+        if self.nesterov:
+            for n, (v, p) in enumerate(zip(velocity, params)):
+                a_t = p - self.m * v
+                params[n] = a_t
 
-            if self.nesterov:
-                p_t = param + self.momentum * velocity - lr * grad
-            else:
-                p_t = param + velocity
+        grads = aet.grad(cost, params, disconnected_inputs="ignore")
 
-            updates.append((m, velocity))
+        for v, param, grad in zip(velocity, params, grads):
+            v_t = self.m * v + lr * grad
+            p_t = param - v_t
+
+            updates.append((v, v_t))
             updates.append((param, p_t))
 
         return updates
