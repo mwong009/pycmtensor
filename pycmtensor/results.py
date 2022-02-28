545f22754089e0324d1f13333d1549be4d181825
Melvin Wong <m.j.w.wong@tue.nl>
2022-02-28 23:51:54 +0100

    feat(time_format): enable logging of build and estimation time

--- pycmtensor/results.py
+++ pycmtensor/results.py
@@ -10,6 +10,15 @@ from pycmtensor.functions import bhhh, hessians
 from pycmtensor.statistics import *
 
 
+def time_format(seconds):
+    minutes, seconds = divmod(round(seconds), 60)
+    if minutes >= 60:
+        hours, minutes = divmod(minutes, 60)
+    else:
+        hours = 0
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+
 class Results:
     def __init__(self, model, database, show_weights=False):
         """Generate the output results to stdout
