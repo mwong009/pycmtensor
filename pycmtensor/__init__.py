6615c56f5fe9c3973ead424c6edbfca7396ca4d5
Melvin Wong <m.j.w.wong@tue.nl>
2022-03-02 01:20:02 +0100

    feat(config.py): enable pre-writing of .aesararc config file on module load
    Also sets the environment variable OMP_NUM_THREADS and MKL_NUM_THREADS.

--- pycmtensor/__init__.py
+++ pycmtensor/__init__.py
@@ -3,5 +3,10 @@
 __author__ = """Melvin Wong"""
 __version__ = "0.5.0b0"
 
+from pycmtensor import config
+
+config.generate_config_file()
+config.set_NUM_THREADS()
+
 from pycmtensor.database import *
 from pycmtensor.pycmtensor import *
