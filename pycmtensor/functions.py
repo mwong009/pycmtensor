fa6547ce862b984addf163a1399abfde6378c68c
Melvin Wong <m.j.w.wong@tue.nl>
2022-03-07 23:43:58 +0100

    fix(functions): makes avail in logit() an optional parameter

--- pycmtensor/functions.py
+++ pycmtensor/functions.py
@@ -1,65 +1,114 @@
 # functions.py
+"""Module containing tensor functions."""
 
 import aesara.tensor as aet
 import numpy as np
 from scipy import linalg
 
+from pycmtensor import logger as log
 
-def logit(utility, avail):
-    """Logit function, with availability conditions
+
+def logit(utility: list, avail: list = None):
+    """Logit function, with availability conditions.
 
     Args:
-        utility (list): List of utility equations (TensorVariable)
-        avail (list): List of availability cond. (TensorVariable)
+        utility (list): List of utility equations.
+        avail (list): List of availability conditions. If not availability is provided, defaults to 1 for all availabilities.
 
     Returns:
-        TensorVariable: A NxN probability vectors corresponding to the choice index
+        TensorVariable: A NxN probability vectors corresponding to the choice index.
     """
+
     if isinstance(utility, (list, tuple)):
-        assert len(utility) == len(
-            avail
-        ), f"{utility} must have the same length as {avail}"
+        if (avail != None) and (len(utility) != len(avail)):
+            msg = f"{utility} must have the same length as {avail}"
+            log.error(msg)
+            raise ValueError(msg)
         U = aet.stacklists(utility).flatten(2)
     else:
         U = utility
 
-    AV = aet.stacklists(avail)
-
-    assert U.ndim == AV.ndim
-
-    prob = aet.nnet.softmax(U, axis=0) * AV
+    prob = aet.nnet.softmax(U, axis=0)
+    if avail != None:
+        AV = aet.stacklists(avail)
+        assert U.ndim == AV.ndim
+        prob = prob * AV
     return prob / aet.sum(prob, axis=0, keepdims=1)
 
 
 def neg_loglikelihood(prob, y):
-    nll = -aet.mean(aet.log(prob)[y, aet.arange(y.shape[0])])
+    """Symbolic description of how to compute the average negative loglikelihood.
+
+    Args:
+        prob (TensorVariable): Function describing the choice probabilites.
+        y (TensorVariable): The ``TensorVariable`` symbol referencing the choice column.
+
+    Returns:
+        TensorVariable: a symbolic description of the average negative loglikelihood.
+    """
+    nll = -aet.mean(full_loglikelihood(prob, y))
     return nll
 
 
 def full_loglikelihood(prob, y):
-    ll = -neg_loglikelihood(prob, y) * y.shape[0]
-    # ll = aet.sum(aet.log(prob)[y, aet.arange(y.shape[0])])
+    """Symbolic description of how to compute the full loglikelihood.
+
+    Args:
+        prob (TensorVariable): Function describing the choice probabilites.
+        y (TensorVariable): The ``TensorVariable`` symbol referencing the choice column.
+
+    Returns:
+        TensorVariable: a symbolic description of the full loglikelihood.
+    """
+    ll = aet.log(prob)[y, aet.arange(y.shape[0])]
     return ll
 
 
 def errors(prob, y):
-    # symbolic description of how to compute prediction as a class
+    """Symbolic description of how to compute prediction as a class.
+
+    Args:
+        prob (TensorVariable): Function describing the choice probabilites.
+        y (TensorVariable): The ``TensorVariable`` symbol referencing the choice column.
+
+    Raises:
+        TypeError: ``y`` should have the same shape as ``pred``.
+        NotImplementedError: ``y`` should be an ``int`` Type.
+
+    Returns:
+        TensorVariable: a symbolic description of the prediction error.
+    """
     if prob.ndim > 2:
         prob = aet.reshape(prob, [prob.shape[0], y.shape[0], -1])
         prob = aet.mean(prob, axis=-1)
     pred = aet.argmax(prob, axis=0)
 
     if y.ndim != pred.ndim:
-        raise TypeError(
-            "y should have the same shape as pred", ("y", y.type, "pred", pred.type)
-        )
+        msg = f"y should have the same shape as pred. y.ndim: {y.ndim}, pred.ndim: {pred.ndim}"
+        log.error(msg)
+        raise ValueError(msg)
     if y.dtype.startswith("int"):
         return aet.mean(aet.neq(pred, y))
     else:
-        raise NotImplementedError()
+        raise NotImplementedError(f"y should be an int Type", ("y.dtype:", y.dtype))
 
 
 def hessians(prob, y, params):
+    """Compute the hessians of the loglikelihood cost function.
+
+    Args:
+        prob (TensorVariable): Function describing the choice probabilites.
+        y (TensorVariable): The ``TensorVariable`` symbol referencing the choice column.
+        params (list): list of params to compute the gradients.
+
+    Returns:
+        Function: returns an Aesara ``function``.`` Calling the return value with no arguments computes the hessian function.
+
+    Note:
+        Parameters with status=1 are ignored.
+
+        This is used as an internal function call.
+    """
     params = [p() for p in params if (p.status != 1)]
     for p in params:
         assert p.ndim == 0, f"{p.name}, ndim={p.ndim}"
@@ -74,9 +123,27 @@ def hessians(prob, y, params):
 
 
 def bhhh(prob, y, params):
+    """Compute the BHHH of the loglikelihood cost function.
+
+    Args:
+        prob (TensorVariable): Function describing the choice probabilites.
+        y (TensorVariable): The ``TensorVariable`` symbol referencing the choice column
+        params (list): list of params to compute the gradients.
+
+    Returns:
+        Function: returns an Aesara ``function``. Calling the return value with no arguments computes the BHHH function.
+
+    Note:
+        Parameters with status=1 are ignored.
+
+        This is used as an internal function call.
+    """
     params = [p() for p in params if (p.status != 1)]
     for p in params:
-        assert p.ndim == 0, f"{p.name}, ndim={p.ndim}"
+        if p.ndim != 0:
+            msg = f"{p.name} is not a valid {p.ndim}-diamension Beta parameter."
+            log.error(msg)
+            raise ValueError(msg)
     grads = aet.grad(full_loglikelihood(prob, y), params, disconnected_inputs="ignore")
     _bh = aet.outer(aet.as_tensor_variable(grads), aet.as_tensor_variable(grads).T)
     return _bh
