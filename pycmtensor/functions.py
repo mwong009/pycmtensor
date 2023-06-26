fix: update hessian matrix and bhhh algorithm functions

pycmtensor/functions.py
@@ -3,6 +3,7 @@
 from ctypes import util
 from typing import Union
 
+import aesara
 import aesara.tensor as aet
 import aesara.tensor.nlinalg as nlinalg
 import numpy as np
@@ -11,6 +12,7 @@ from aesara.tensor.var import TensorVariable
 
 from pycmtensor.expressions import Beta, Param
 
+from .data import FLOATX
 from .logger import error, log
 
 __all__ = [
@@ -23,7 +25,6 @@ __all__ = [
     "kl_multivar_norm",
     "errors",
     "hessians",
-    "bhhh",
     "gnorm",
 ]
 
@@ -287,11 +288,11 @@ def errors(prob: TensorVariable, y: TensorVariable):
         raise NotImplementedError(f"y should be int32 or int64", ("y.dtype:", y.dtype))
 
 
-def hessians(ll: TensorVariable, params: list[Beta]):
-    """Symbolic representation of the Hessian matrix given the log likelihood.
+def hessians(cost: TensorVariable, params: list[Beta]):
+    """Symbolic representation of the 2nd order Hessian matrix given the neg. log likelihood.
 
     Args:
-        ll (TensorVariable): the loglikelihood to compute the gradients over
+        cost (TensorVariable): the neg loglikelihood to compute the gradients over
         params (list): list of params to compute the gradients over
 
     Returns:
@@ -303,7 +304,7 @@ def hessians(ll: TensorVariable, params: list[Beta]):
     if not isinstance(params, list):
         raise TypeError(f"params is not list instance. type(params)={type(params)}")
     params = [p() for p in params if (p.status != 1)]
-    grads = aet.grad(ll, params, disconnected_inputs="ignore")
+    grads = aet.grad(-cost, params, disconnected_inputs="ignore")
     mat = aet.as_tensor_variable(np.zeros((len(grads), len(grads))))
     for i in range(len(grads)):
         mat = aet.set_subtensor(
@@ -313,16 +314,15 @@ def hessians(ll: TensorVariable, params: list[Beta]):
     return mat
 
 
-def bhhh(ll: TensorVariable, params: list[Beta]):
-    """Symbolic representation of the Berndt-Hall-Hall-Hausman (BHHH) algorithm given
-    the log likelihood.
+def gradient_vector(cost: TensorVariable, params: list[Beta]):
+    """Symbolic representation of the 1st order gradient vector given the neg. log likelihood.
 
     Args:
-        ll (TensorVariable): the loglikelihood to compute the gradients over
+        cost (TensorVariable): the neg loglikelihood to compute the gradients over
         params (list): list of params to compute the gradients over
 
     Returns:
-        TensorVariable: the outer product of the gradient with ndim=2
+        TensorVariable: the gradient vector
 
     Note:
         Parameters with status=1 are ignored.
@@ -334,9 +334,8 @@ def bhhh(ll: TensorVariable, params: list[Beta]):
     if isinstance(params, dict):
         params = list(params.values())
     params = [p() for p in params if (p.status != 1)]
-    grads = aet.grad(ll, params, disconnected_inputs="ignore")
-    mat = aet.outer(aet.as_tensor_variable(grads), aet.as_tensor_variable(grads).T)
-    return mat
+    grads = aet.grad(-cost, params, disconnected_inputs="ignore")
+    return aet.as_tensor_variable(grads)
 
 
 def gnorm(cost: TensorVariable, params: list[Beta]):

pycmtensor/pycmtensor.py
@@ -13,7 +13,7 @@ from aesara.tensor.var import TensorVariable
 from pycmtensor import config, rng
 
 from .expressions import Beta, ExpressionParser, Weight
-from .functions import bhhh, errors, gnorm, hessians
+from .functions import errors, gnorm, gradient_vector, hessians
 from .logger import debug, info, warning
 from .optimizers import Adam, Optimizer
 from .results import Results
@@ -163,17 +163,17 @@ class PyCMTensorModel:
         self.H = function(
             name="Hessian matrix",
             inputs=self.inputs,
-            outputs=hessians(self.ll, self.betas),
+            outputs=hessians(self.cost, self.betas),
         )
 
-    def model_BHHH(self):
-        """Loads the function to ``self.BHHH()`` to calculate the Berndt-Hall-Hall-
-        Hausman (BHHH) approximation.
+    def model_G(self):
+        """Loads the function to ``self.G()`` to calculate the first order gradients
+        of the model.
         """
-        self.BHHH = function(
+        self.G = function(
             name="BHHH matrix",
             inputs=self.inputs,
-            outputs=bhhh(self.ll, self.betas),
+            outputs=gradient_vector(self.cost, self.betas),
         )
 
     def model_gnorm(self):
@@ -314,6 +314,8 @@ class PyCMTensorModel:
 
         self.betas = self.results.betas
         self.weights = self.results.weights
+        self.gradients = self.G(*db.train_data)
+
         self.results.n_train_samples = db.n_train_samples
         self.results.n_valid_samples = db.n_valid_samples
         self.results.n_params = self.n_params
@@ -323,7 +325,9 @@ class PyCMTensorModel:
         # statistical analysis step
         self.results.gnorm = self.gradient_norm(*db.train_data)
         self.results.hessian_matrix = self.H(*db.train_data)
-        self.results.bhhh_matrix = self.BHHH(*db.train_data)
+        self.results.bhhh_matrix = (
+            np.outer(self.gradients, self.gradients) * db.n_train_samples
+        )
 
     def export_to_pickle(self, f):
         """to be removed in 1.4.0"""

pycmtensor/statistics.py
@@ -19,16 +19,16 @@ __all__ = [
 
 
 def variance_covariance(h):
-    """Returns the var-covar matrix given the Hessian (``h``)"""
-    return -nlinalg.pinv(nan2num(h)).eval()
+    """Returns the variance covariance matrix given the Hessian (``h``)"""
+    return nlinalg.pinv(nan2num(-h)).eval()
 
 
-def rob_variance_covariance(h, bhhh):
-    """Returns the rob. var-covar matrix given the Hessian (``h``) and the BHHH
-    matrix (``bhhh``)
+def rob_variance_covariance(h, bh):
+    """Returns the rob. var-covar matrix given the Cramer-Rao bound (``h``) and the BHHH
+    matrix (``bh``)
     """
-    var_covar = variance_covariance(h)
-    return var_covar.dot(bhhh.dot(var_covar))
+    cr = variance_covariance(h)
+    return cr.dot(bh.dot(cr))
 
 
 def t_test(stderr, params):
@@ -44,7 +44,12 @@ def p_value(stderr, params):
 
 
 def stderror(h, params):
-    """Returns the standard error of ``params`` given the Hessian (``h``)"""
+    """Returns the standard error of ``params`` given the Hessian (``h``)
+
+    The std err is calculated as the square root of the variance covariance
+    matrix.
+
+    """
     params = [p() for p in params if (p.status != 1)]
     varCovar = variance_covariance(h)
     stdErr = []
@@ -57,13 +62,13 @@ def stderror(h, params):
     return stdErr
 
 
-def rob_stderror(h, bhhh, params):
+def rob_stderror(h, bh, params):
     """Returns the rob. standard error of ``params`` given the Hessian (``h``) and the
-    BHHH matrix (``bhhh``)
+    BHHH matrix (``bh``)
     """
     params = [p() for p in params if (p.status != 1)]
     varCovar = variance_covariance(h)
-    robVarCovar = varCovar.dot(bhhh.dot(varCovar))
+    robVarCovar = varCovar.dot(bh.dot(varCovar))
     robstderr = []
     for i in range(len(params)):
         if robVarCovar[i, i] < 0:
@@ -88,9 +93,9 @@ def correlation_matrix(h):
     return mat
 
 
-def rob_correlation_matrix(h, bhhh):
+def rob_correlation_matrix(h, bh):
     """Returns the correlation matrix given the Hessian and the BHHH matrix"""
-    rob_var_covar = rob_variance_covariance(h, bhhh)
+    rob_var_covar = rob_variance_covariance(h, bh)
     rd = np.diag(rob_var_covar)
     if (rd > 0).all():
         diag = np.diag(np.sqrt(rd))