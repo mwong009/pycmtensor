8e2e34ea5265d5ea2b1fdf61f5c40bacf9222784
Melvin Wong <m.j.w.wong@tue.nl>
2023-07-17 23:36:53 +0200

    feat(dataset.py): added list(`str`) as tensor arguments for `train_dataset()` and `valid_dataset()`

--- pycmtensor/dataset.py
+++ pycmtensor/dataset.py
@@ -1,7 +1,10 @@
 # dataset.py
 # converts pandas dataframe into an xarray dataset
 
+from typing import Union
+
 import aesara.tensor as aet
+from aesara.tensor.var import TensorVariable
 
 from pycmtensor import config
 
@@ -14,24 +17,43 @@ class Dataset:
     def __init__(self, df, choice):
         """Base PyCMTensor Dataset class object
 
+        This class stores the data in an array format, and a symbolic tensor reference
+        variable object. To call the tensor variable, we invoke the label of the
+        variable as an item in the Dataset class, like so:
+        ```python
+        ds = Dataset(df=df, choice="choice")
+        return ds["label_of_variable"]
+        ```
+
+        To call the data array, we use the `train_dataset()` or `valid_dataset()`
+        method. See method reference for info about the arguments. For example:
+        ```python
+        # to get the data array for variable "time"
+        arr = ds.train_dataset(ds["time"])
+        ```
+
         Args:
             df (pandas.DataFrame): the pandas dataframe object to load
             choice (str): the name of the choice variable
 
         Attributes:
             n (int): total number of rows in the dataset
-            x (list[TensorVariable]): a list of (input) `TensorVariable` objects to
-                build the tensor expression from
+            x (list[TensorVariable]): the full list of (input) `TensorVariable` objects
+                to build the tensor expression from
             y (TensorVariable): the output (choice) `TensorVariable` object
-            scale (dict): a dictionary of `float` values to store the scaling factor used for each variable
+            scale (dict): a dictionary of `float` values to store the scaling factor
+                used for each variable
             choice (str): the name of the choice variable
-            ds (dict): a dictionary of `np.ndarray` to store the values of each variable
+            ds (dict): a dictionary of `numpy.ndarray` to store the values of each
+                variable
             split_frac (float): the factor used to split the dataset into training and
                 validation datasets
-            train_index (list):
-            valid_index (list):
-            n_train (int):
-            n_valid (int):
+            train_index (list): the list of values of the indices of the training
+                dataset
+            valid_index (list): the list of values of the indices of the validation
+                dataset
+            n_train (int): the size of the training dataset
+            n_valid (int): the size of the validation dataset
 
         Example:
             Example initalization of a pandas dataset:
@@ -134,7 +156,7 @@ class Dataset:
                 raise KeyError
 
     def scale_variable(self, variable, factor):
-        """Multiply values of the `variable` by factor 1/factor.
+        """Multiply values of the `variable` by $1/\\textrm{factor}$.
 
         Args:
             variable (str): the name of the variable or a list of variable names
@@ -144,24 +166,36 @@ class Dataset:
         self.scale[variable] = self.scale[variable] * factor
 
     def split(self, frac):
-        """TODO"""
-        n = round(self.n * frac)
+        """Method to split dataset into training and validation subsets
+
+        Args:
+            frac (float): the fraction to split the dataset into the training set. The training set will be indexed from `0` to `frac` $\\times$ `Dataset.n`. The validation dataset will be from the last index of the training set to the last row of the dataset.
+
+        Note:
+            The actual splitting of the dataset is done during the training procedure,
+            or when invoking the `train_dataset()` or `valid_dataset()` methods
+
+        """
+
         self.split_frac = frac
         info(f"n_train_samples:{self.n_train} n_valid_samples:{self.n_valid}")
 
     def _dataset_slice(self, tensors, index, batch_size, shift, n_index):
-        """Internal method call for self.train_dataset or self.valid_dataset
+        """Internal method call for self.train_dataset or self.valid_dataset"""
 
-        Args:
-            tensors (TensorVariable): tensor or list of tensors
-            index (int):
-            batch_size (int):
-            shift (int):
-            n_index (list): list of index values of the [train|valid] dataset
-        """
         if not isinstance(tensors, list):
             tensors = [tensors]
 
+        # check if all tensors are of the same type tensors
+        if all(isinstance(t, TensorVariable) for t in tensors):
+            pass
+        # if tensor inputs are list of strings, convert them to tensors
+        elif all(isinstance(t, str) for t in tensors):
+            tensors = [self[t.name] for t in tensors]
+        else:
+            raise TypeError(f"Multiple types found in {tensors}.")
+
+        # retrieve tensor names
         tensor_names = [t.name for t in tensors]
         for name in tensor_names:
             if name not in list(self.ds):
@@ -183,12 +217,57 @@ class Dataset:
         return _ds
 
     def train_dataset(self, variables, index=None, batch_size=None, shift=None):
-        """Return a slice of the training dataset with the sequence matching the list of variables"""
+        """Returns a slice of the (or the full) training data array with the sequence
+        matching the list of variables.
+
+        Args:
+            variables (Union[list, str, TensorVariable]): a tensor, label, or list of
+                tensors or list of labels
+            index (int): the start of the slice of the data array. If `None` is given,
+                returns the full data array.
+            batch_size (int): length of the slice. If `None` is given, returns the
+                index from `index` to `N` where `N` is the length of the array.
+            shift (int): the offset of the slice between `0` and `batch_size`. If
+                `None` is given, `shift=0`.
+
+        Returns:
+            (list): a list of array object(s) corresponding to the input variables
+
+        !!! Example
+            How to retrieve data array from Dataset:
+            ```python
+            ds = Dataset(df, choice="choice")
+
+            # index "age" and "location" data arrays
+            return ds.train_dataset([ds["age"], ds["location"]])
+
+            # similar result
+            return ds.train_dataset(["age", "location"])
+            ```
+        """
+
         n_index = self.train_index
+
         return self._dataset_slice(variables, index, batch_size, shift, n_index)
 
     def valid_dataset(self, variables, index=None, batch_size=None, shift=None):
-        """Return a slice of the valid dataset with the sequence matching the list of variables"""
+        """Returns a slice of the (or the full) validation data array with the sequence
+        matching the list of variables.
+
+        Args:
+            variables (Union[list, str, TensorVariable]): a tensor, label, or list of
+                tensors or list of labels
+            index (int): the start of the slice of the data array. If `None` is given,
+                returns the full data array.
+            batch_size (int): length of the slice. If `None` is given, returns the
+                index from `index` to `N` where `N` is the length of the array.
+            shift (int): the offset of the slice between `0` and `batch_size`. If
+                `None` is given, `shift=0`.
+
+        Returns:
+            (list): a list of array object(s) corresponding to the input variables
+        """
+
         n_index = self.valid_index
 
         return self._dataset_slice(variables, index, batch_size, shift, n_index)
