1cd396c27e449948195c789db34a0a0457cccee0
Melvin <m.j.w.wong@tue.nl>
2023-11-24 15:48:10 +0100

    feat(utils): add new function to display numbers as human readable format

--- pycmtensor/utils.py
+++ pycmtensor/utils.py
@@ -9,3 +9,14 @@ def time_format(seconds):
     else:
         hours = 0
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+
+def human_format(number):
+    number = float(f"{number:.3g}")
+    magnitude = 0
+    while abs(number) >= 1000:
+        magnitude += 1
+        number /= 1000.0
+    return (
+        f"{number}".rstrip("0").rstrip(".") + ["", "K", "M", "B", "T", "P"][magnitude]
+    )
