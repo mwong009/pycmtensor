a279a693139a8837ba836cc13202e158b5340762
Melvin <m.j.w.wong@tue.nl>
2023-11-27 15:44:30 +0100

    test(statistics): add tests for statistics

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -42,3 +42,52 @@ def test_p_value():
     params = {"param1": np.array([1, 2, 3]), "param2": np.array([4, 5, 6])}
     expected = [0.0, 0.0]
     assert pycmtensor.statistics.p_value(stderr, params) == expected
+
+
+def test_rob_stderr():
+    hessian = np.array([[2, -1], [-1, 2]])
+    bhhh = np.array([[2.1, 0.2], [2.3, 0.238]])
+    params = {"param1": np.array([1]), "param2": np.array([2])}
+
+    result = pycmtensor.statistics.rob_stderror(hessian, bhhh, params)
+    assert np.allclose(result, [1.231, 0.946], rtol=1e-3)
+    assert isinstance(result, list)
+
+    hessian = np.array([[2, -1], [-1, 2]])
+    params = {"param1": np.array(0), "param2": np.array(0)}
+
+    result = pycmtensor.statistics.rob_stderror(hessian, bhhh, params)
+    assert result == [np.nan, np.nan]
+
+    hessian = np.array([[-1.2, 3.11], [7.8, -0.005]])
+    bhhh = np.array([[-0.4, 0.20], [0.1, -0.10]])
+    params = {"param1": np.array(1), "param2": np.array(2)}
+    float_max = np.finfo(float).max
+
+    result = pycmtensor.statistics.rob_stderror(hessian, bhhh, params)
+    assert result == [float_max, float_max]
+
+
+def test_correlation_matrix():
+    hessian = np.array([[-2.2, -0.1], [-0.2, -3.3]])
+    expected = np.array([[1, -0.037113], [-0.074227, 1]])
+    result = pycmtensor.statistics.correlation_matrix(hessian)
+    assert np.allclose(result, expected, rtol=1e-3)
+
+    hessian = np.array([[0, 0], [0, 0]])
+    result = pycmtensor.statistics.correlation_matrix(hessian)
+    float_max = np.finfo(float).max
+    assert np.allclose(result, float_max, rtol=1e-3)
+
+
+def test_rob_correlation_matrix():
+    hessian = np.array([[-2.2, -0.1], [-0.2, -3.3]])
+    bhhh = np.array([[2.1, 0.2], [2.3, 0.238]])
+    expected = np.array([[1, 0.20091893], [4.18978917, 1]])
+    result = pycmtensor.statistics.rob_correlation_matrix(hessian, bhhh)
+    assert np.allclose(result, expected, rtol=1e-3)
+
+    hessian = np.array([[0, 0], [0, 0]])
+    result = pycmtensor.statistics.rob_correlation_matrix(hessian, bhhh)
+    float_max = np.finfo(float).max
+    assert np.allclose(result, float_max, rtol=1e-3)
